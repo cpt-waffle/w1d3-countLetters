6dac1bc9a9c2b17017cafb199107d25720cef593
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-05-30 22:28:13 +0000

    split and join takes alot of resources to run, changed code to not use it

--- countLetters.js
+++ countLetters.js
@@ -1,19 +1,19 @@
 function countLetters(some_string)
  {
  var myObject =  {}
- var noSpaces = some_string.split(" ").join("");
+ //var noSpaces = some_string.split(" ").join("");
 
  //loop to add each letter as a key
- for (var i of noSpaces)
+ for (var i of some_string)
  {
-
+  if (i != ' ')
+  {
     if (typeof myObject[i] == 'undefined')
       myObject[i] = 1;
     else
       myObject[i]++;
-
+  }
  }
-
  //console.log(Object.keys(myObject));
  return myObject;
 };
